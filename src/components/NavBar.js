f17c23175a4d98f536b81692fd3ec4efa342cf93
iemeli <eemeli@paalimaki.fi>
2022-03-13 15:58:58 +0200

    Add NavBar some initial style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,35 @@
 import { Link } from 'react-router-dom'
+import '../css/NavBar.css'
+import logo from '../images/logopohja.jpg'
 
 const NavBar = () => {
-  const padding = {
-    padding: 20,
-  }
   return (
-    <div>
-      <Link style={padding} to="/">
-        koti
-      </Link>
-      <Link style={padding} to="/practice">
-        treenaa
-      </Link>
-      <Link style={padding} to="/Revise">
-        kertaa
-      </Link>
-      <Link style={padding} to="/login">
-        kirjautuminen
-      </Link>
-      <Link style={padding} to="/register">
-        luo tili
-      </Link>
-      {/* <Link style={padding} to="/cards">cards</Link> */}
-      {/* {user
+    <div className="NavBar">
+      <div className="NavBarItemsContainer">
+        <Link className="NavBarItem" to="/">
+          koti
+        </Link>
+        <Link className="NavBarItem" to="/practice">
+          treenaa
+        </Link>
+        <Link className="NavBarItem" to="/Revise">
+          kertaa
+        </Link>
+        <Link className="NavBarItem" to="/login">
+          kirjautuminen
+        </Link>
+        <Link className="NavBarItem" to="/register">
+          luo tili
+        </Link>
+        {/* <Link className="NavBarItem" to="/cards">cards</Link> */}
+        {/* {user
           ? <em>{user} logged in</em>
-          : <Link style={padding} to="/login">login</Link>
+          : <Link className="NavBarItem" to="/login">login</Link>
         } */}
+      </div>
+      <div className="logoContainer">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
     </div>
   )
 }
