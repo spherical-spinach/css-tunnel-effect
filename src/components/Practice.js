3aa447c146ab54c5026e4d366401be7c4bad4614
iemeli <eemeli@paalimaki.fi>
2022-04-05 15:01:06 +0300

    Make accordion menu's to Practice view

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,15 +1,22 @@
-import Courses from './Courses'
+// import Courses from './Courses'
+
+import Accordion from './Accordion'
+import '../css/Practice.css'
 
 const Practice = () => {
   return (
-    <div>
-      <h2>Omat treenit</h2>
+    <div className="practiceContainer">
+      {/* <h2>Omat treenit</h2>
       <p>
         Et ole kirjautuneena sisään. Jos haluat treenin jäävän muistiin,
         kirjaudu sisään. Jos sinulla ei vielä ole tiliä, rekisteröidy
         käyttäjäksi täällä.{' '}
-      </p>
-      <Courses></Courses>
+      </p> */}
+      {/* <Courses /> */}
+      <Accordion title="KURSSI" />
+      <Accordion title="OSA-ALUE" />
+      <Accordion title="KYSYMYSTEN MÄÄRÄ" />
+      <Accordion title="KYSYMYKSET" />
     </div>
   )
 }
