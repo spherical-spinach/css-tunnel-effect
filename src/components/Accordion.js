db4af49d8e36d8b66265f27cde5a3bcf0142703e
Sini <siniesofia@protonmail.com>
2022-05-24 14:30:38 +0300

    Disable eslint rules and fix eslint errors

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,29 +3,38 @@ import { useSelector } from 'react-redux'
 import '../css/Accordion.css'
 
 function returnCourse(title, course, setCourse) {
-  if (title == 'KURSSI') {
+  if (title === 'KURSSI') {
     return (
-      <div className="accordionContent" onClick={() => setCourse(course.name)}>
+      <div
+        key={course.id}
+        className="accordionContent"
+        onClick={() => setCourse(course.name)}
+        onKeyPress={() => setCourse(course.name)}
+        role="button"
+        tabIndex={0}
+      >
         <p>{course.name}</p>
       </div>
     )
   }
+  return null
 }
 
 function returnParts(title, part, course) {
   console.log('course', course)
   console.log('part', part.courseId.name)
-  if (title == 'OSA-ALUE') {
+  if (title === 'OSA-ALUE') {
     return (
-      <div className="accordionContent">
-        <p>{part.name}</p>
+      <div key={part.id} className="accordionContent">
+        <p key={part.id}>{part.name}</p>
       </div>
     )
   }
+  return null
 }
 
 function returnAmount(title) {
-  if (title == 'KYSYMYSTEN MÄÄRÄ') {
+  if (title === 'KYSYMYSTEN MÄÄRÄ') {
     return (
       <div className="accordionContent">
         <p>5</p>
@@ -36,30 +45,28 @@ function returnAmount(title) {
       </div>
     )
   }
+  return null
 }
 
 const Accordion = ({ title }) => {
   const [show, setShow] = useState(false)
   const [course, setCourse] = useState(null)
-  // console.log('course', course)
   const courses = useSelector(state => state.courses)
   const parts = useSelector(state => state.parts)
-  const joku = useSelector(state => state.course)
-  console.log('joku', joku)
-  // console.log('parts', parts)
+
   let triangleClasses = 'arrow-down'
   if (show) {
     triangleClasses = 'arrow-down show'
   }
   return (
     <div className="accordionContainer">
-      <div className="accordionHead" onClick={() => setShow(!show)} s>
+      <div className="accordionHead" onClick={() => setShow(!show)}>
         <h2>{title}</h2>
         <div className={triangleClasses} />
       </div>
       {show && (
         <div>
-          {courses.map(course => returnCourse(title, course, setCourse))}
+          {courses.map(course2 => returnCourse(title, course2, setCourse))}
           {parts.map(part => returnParts(title, part, course))}
           {returnAmount(title, setShow, show)}
         </div>
--- src/components/Practice.js
+++ src/components/Practice.js
@@ -9,18 +9,17 @@ const Practice = () => {
 
   return (
     <div className="practiceContainer">
-      {/* <h2>Omat treenit</h2>
-      <p>
-        Et ole kirjautuneena sisään. Jos haluat treenin jäävän muistiin,
-        kirjaudu sisään. Jos sinulla ei vielä ole tiliä, rekisteröidy
-        käyttäjäksi täällä.{' '}
-      </p> */}
-      {/* <Courses /> */}
       <Accordion title="KURSSI" />
       <Accordion title="OSA-ALUE" />
       <Accordion title="KYSYMYSTEN MÄÄRÄ" />
       {/* <Accordion title="KYSYMYKSET" /> */}
-      <div className="practice2Button" onClick={() => navigate('/practice2')}>
+      <div
+        className="practice2Button"
+        onClick={() => navigate('/practice2')}
+        onKeyPress={() => navigate('/practice2')}
+        role="button"
+        tabIndex={0}
+      >
         <p>Treenaa 2 -näkymä</p>
       </div>
     </div>
