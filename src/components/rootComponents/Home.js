d33f8026d87703bb64ae2af1a8bf6ceafc14f5db
Sini <siniesofia@protonmail.com>
2022-10-27 16:35:18 +0300

    Style homepage

--- src/components/rootComponents/Home.js
+++ src/components/rootComponents/Home.js
@@ -5,18 +5,19 @@ import { useSelector } from 'react-redux'
 // import textLogo from '../../images/textLogo.png'
 import { selectUser } from '../../reducers/userReducer'
 import laaketiede from '../../images/laaketiede.png'
+import varjovalmennuslogo from '../../images/varjovalmennuslogo.png'
 
 const Home = () => {
   const navigate = useNavigate()
   const user = useSelector(selectUser)
 
   return (
-    <div>
-      <div className="paasykoetreenitContainer">
-        <h2 className="headerheader">PÄÄSYKOETREENIT</h2>
-      </div>
+    <div className="homeViewContainer">
+      {/* <div className="topContainer">
+        <h2 className="header">PÄÄSYKOETREENIT</h2>
+      </div> */}
       <div className="homeContainer">
-        {/* <h2 className="header">PÄÄSYKOETREENIT</h2> */}
+        <h2 className="header">PÄÄSYKOETREENIT</h2>
         {user ? (
           <h4 className="mainText">
             Olet kirjautunut sisään käyttäjänimellä {`'${user?.username}'`}
@@ -33,22 +34,47 @@ const Home = () => {
           role="button"
           tabIndex={0}
         >
-          <h1 className="middleButtonText">ALOITA</h1>
+          <h1 className="middleButtonText">TREENAA</h1>
         </div>
-        {!user && (
-          <p className="mainText">
-            Eikö sinulla ole tunnusta? <Link to="/register">Rekisteröidy</Link>
-          </p>
+        {user ? (
+          <p className="mainText">Katso tilastot omista treeneistäsi.</p>
+        ) : (
+          <div>
+            <p className="highlightedText">Eikö sinulla ole tunnusta? </p>
+            <p className="mainText">
+              <Link to="/register">Rekisteröidy</Link>, jos haluat treenisi
+              jäävän talteen ja saada kerrattavaa.
+            </p>
+            <p className="infoText">
+              Pääsykoetreenit tarjoaa ilmaisia kysymyksiä ja vastauksia
+              pääsykoemateriaaleista. Lue lisää
+            </p>
+          </div>
         )}
         {/* <div>
           <Link to="/addQuestions">Lisää kysymyksiä</Link>
         </div> */}
+        {/* <h4 className="mainText">
+          Pääsykoetreenit-sivustolla saat ilmaiseksi harjoiteltavaksi kysymyksiä
+          eri alojen pääsykoemateriaaleista. Lue lisää
+        </h4> */}
       </div>
-      <div className="headerContainer">
-        <h2 className="headerheader">KURSSIT</h2>
+      <div className="boxContainer">
+        <h2 className="header">YHTEISTYÖSSÄ VARJOVALMENNUS</h2>
+        <img
+          className="logoImage"
+          src={varjovalmennuslogo}
+          alt="Varjovalmennus logo"
+        />
       </div>
-      <div className="courseContainer">
-        <h2 className="coursesHeader">LÄÄKETIEDE</h2>
+      <div>
+        <h2 className="courses">KURSSIT</h2>
+      </div>
+      {/* <div>
+        <img className="courseImage" src={laaketiede} alt="course" />
+      </div> */}
+      <div className="coursesContainer">
+        <h2 className="subHeader">Lääketiede</h2>
         <img className="courseImage" src={laaketiede} alt="course" />
       </div>
     </div>
