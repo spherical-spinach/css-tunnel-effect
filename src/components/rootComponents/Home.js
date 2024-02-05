5537be302af846ac1d2233d20dc0e01a144fd5bf
iemeli <eemeli.paalimaki.work@protonmail.com>
2024-02-05 20:38:08 +0200

    Style home view

--- src/components/rootComponents/Home.js
+++ src/components/rootComponents/Home.js
@@ -1,34 +1,14 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../../css/Home.css'
 import '../../css/HomeNew.css'
 
-const Home = () => {
-  const navigate = useNavigate()
-
-  return (
-    <div className="homeViewContainer">
-      <div className="homeContainer">
-        <h2 className="header">List of dark patterns</h2>
-
-        <div className="homeListItemContainer">
-          <div className="arrow-right" />
-          <Link to="/login">Trick wording</Link>{' '}
-          <div
-            className="startButton"
-            onClick={() => navigate('/practice')}
-            onKeyPress={() => navigate('/practice')}
-            role="button"
-            tabIndex={0}
-          >
-            <h1 className="startButtonText">button</h1>
-          </div>
-        </div>
-        <div>
-          <p className="highlightedText"> classname highlightedText</p>
-        </div>
-      </div>
+const Home = () => (
+  <div className="homeViewContainer">
+    <div className="homeContainer">
+      <h2 className="header">List of dark patterns</h2>
+      <Link to="/">Trick wording</Link>{' '}
     </div>
-  )
-}
+  </div>
+)
 
 export default Home
