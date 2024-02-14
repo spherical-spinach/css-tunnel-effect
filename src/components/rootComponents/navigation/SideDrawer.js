e34cc1218552c0d25b06745cb192b92cc37b3230
iemeli <eemeli.paalimaki.work@protonmail.com>
2024-02-14 15:30:13 +0200

    Update sidedrawer style

--- src/components/rootComponents/navigation/SideDrawer.js
+++ src/components/rootComponents/navigation/SideDrawer.js
@@ -1,11 +1,7 @@
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import '../../../css/SideDrawer.css'
-import { selectUser } from '../../../reducers/userReducer'
 
 const SideDrawer = ({ show, drawerToggleClickHandler }) => {
-  const user = useSelector(selectUser)
-
   let drawerClasses = 'sideDrawer'
   if (show) {
     drawerClasses = 'sideDrawer open'
@@ -15,29 +11,13 @@ const SideDrawer = ({ show, drawerToggleClickHandler }) => {
       <ul>
         <li>
           <Link to="/" onClick={drawerToggleClickHandler}>
-            koti
+            LIST
           </Link>
         </li>
         <li>
-          <Link to="/practice" onClick={drawerToggleClickHandler}>
-            treenaa
+          <Link to="/" onClick={drawerToggleClickHandler}>
+            ABOUT
           </Link>
-        </li>
-        <li>
-          <Link to="/login" onClick={drawerToggleClickHandler}>
-            {user ? 'kirjaudu ulos' : 'kirjaudu sisään'}
-          </Link>
-        </li>
-        <li>
-          {user ? (
-            <Link to="/userView" onClick={drawerToggleClickHandler}>
-              {user?.username}
-            </Link>
-          ) : (
-            <Link to="/register" onClick={drawerToggleClickHandler}>
-              luo tili
-            </Link>
-          )}
         </li>
       </ul>
     </nav>
