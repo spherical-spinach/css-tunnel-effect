ce98874b25fc89970005c94e2b20219aab08a4cb
iemeli <eemeli.paalimaki.work@protonmail.com>
2024-03-14 14:35:36 +0200

    Set up gh-pages

--- src/components/rootComponents/List.js
+++ src/components/rootComponents/List.js
@@ -1,15 +1,9 @@
-import { Link } from 'react-router-dom'
 import '../../css/Home.css'
 import Tunnel from './Tunnel'
 
 const List = () => (
   <div className="content-container">
-    <h2 className="header">List of Dark Patterns</h2>
-    <div className="horizontal-line" />
-    <div className="home-list-item">
-      <div className="arrow-right" />
-      <Link to="/trickWording">Trick wording</Link>{' '}
-    </div>
+    <h2 className="header">The Tunnel of Dark Patterns</h2>
     <Tunnel />
   </div>
 )
