d4387694a685163a74e5024cb9781f8c6079f301
iemeli <eemeli.paalimaki.work@protonmail.com>
2024-03-21 12:59:25 +0200

    Add another pulsating circle

--- src/components/rootComponents/Tunnel.js
+++ src/components/rootComponents/Tunnel.js
@@ -31,116 +31,72 @@ const Tunnel = () => {
 
   const circleSize = 400
 
+  const circles = [
+    {
+      id: 1,
+      size: 1,
+      borderStyle: 'solid hidden solid hidden',
+      className: 'circle--rotation',
+    },
+    {
+      id: 2,
+      size: 1.2,
+      borderStyle: 'hidden solid hidden solid',
+      className: 'circle--rotation',
+    },
+    {
+      id: 3,
+      size: 1.4,
+      borderStyle: 'solid hidden solid hidden',
+      className: 'circle--rotation',
+    },
+    {
+      id: 4,
+      size: 1.6,
+      borderStyle: 'hidden solid hidden solid',
+      className: 'circle--rotation',
+    },
+    {
+      id: 5,
+      size: 1.8,
+      borderStyle: 'solid hidden solid hidden',
+      className: 'circle--rotation',
+    },
+    {
+      id: 6,
+      size: 2,
+      borderStyle: 'hidden solid hidden solid',
+      className: 'circle--rotation',
+    },
+    {
+      id: 7,
+      size: 2.2,
+      borderStyle: 'solid hidden solid hidden',
+      className: 'circle--rotation',
+    },
+  ]
+
   return (
     <>
-      <div
-        className="circle circle--rotation"
-        style={{
-          left:
-            documentCenter.x - circleSize / 2 + distanceX / (circleSize / 20),
-          top:
-            documentCenter.y - circleSize / 2 + distanceY / (circleSize / 20),
-          width: circleSize,
-          height: circleSize,
-          borderStyle: 'solid hidden solid hidden',
-        }}
-      />
-      <div
-        className="circle circle--rotation"
-        style={{
-          left:
-            documentCenter.x -
-            circleSize / 1.2 / 2 +
-            distanceX / (circleSize / 1.2 / 20),
-          top:
-            documentCenter.y -
-            circleSize / 1.2 / 2 +
-            distanceY / (circleSize / 1.2 / 20),
-          width: circleSize / 1.2,
-          height: circleSize / 1.2,
-          borderStyle: 'hidden solid hidden solid',
-        }}
-      />
-      <div
-        className="circle circle--rotation"
-        style={{
-          left:
-            documentCenter.x -
-            circleSize / 1.4 / 2 +
-            distanceX / (circleSize / 1.4 / 20),
-          top:
-            documentCenter.y -
-            circleSize / 1.4 / 2 +
-            distanceY / (circleSize / 1.4 / 20),
-          width: circleSize / 1.4,
-          height: circleSize / 1.4,
-          borderStyle: 'solid hidden solid hidden',
-        }}
-      />
-      <div
-        className="circle circle--rotation"
-        style={{
-          left:
-            documentCenter.x -
-            circleSize / 1.6 / 2 +
-            distanceX / (circleSize / 1.6 / 20),
-          top:
-            documentCenter.y -
-            circleSize / 1.6 / 2 +
-            distanceY / (circleSize / 1.6 / 20),
-          width: circleSize / 1.6,
-          height: circleSize / 1.6,
-          borderStyle: 'hidden solid hidden solid',
-        }}
-      />
-      <div
-        className="circle circle--rotation"
-        style={{
-          left:
-            documentCenter.x -
-            circleSize / 1.8 / 2 +
-            distanceX / (circleSize / 1.8 / 20),
-          top:
-            documentCenter.y -
-            circleSize / 1.8 / 2 +
-            distanceY / (circleSize / 1.8 / 20),
-          width: circleSize / 1.8,
-          height: circleSize / 1.8,
-          borderStyle: 'solid hidden solid hidden',
-        }}
-      />
-      <div
-        className="circle circle--rotation"
-        style={{
-          left:
-            documentCenter.x -
-            circleSize / 2 / 2 +
-            distanceX / (circleSize / 2 / 20),
-          top:
-            documentCenter.y -
-            circleSize / 2 / 2 +
-            distanceY / (circleSize / 2 / 20),
-          width: circleSize / 2,
-          height: circleSize / 2,
-          borderStyle: 'hidden solid hidden solid',
-        }}
-      />
-      <div
-        className="circle circle--rotation"
-        style={{
-          left:
-            documentCenter.x -
-            circleSize / 2.2 / 2 +
-            distanceX / (circleSize / 2.2 / 20),
-          top:
-            documentCenter.y -
-            circleSize / 2.2 / 2 +
-            distanceY / (circleSize / 2.2 / 20),
-          width: circleSize / 2.2,
-          height: circleSize / 2.2,
-          borderStyle: 'solid hidden solid hidden',
-        }}
-      />
+      {circles.map(circle => (
+        <div
+          key={circle.id}
+          className={`circle ${circle.className}`}
+          style={{
+            left:
+              documentCenter.x -
+              circleSize / circle.size / 2 +
+              distanceX / (circleSize / circle.size / 20),
+            top:
+              documentCenter.y -
+              circleSize / circle.size / 2 +
+              distanceY / (circleSize / circle.size / 20),
+            width: circleSize / circle.size,
+            height: circleSize / circle.size,
+            borderStyle: circle.borderStyle,
+          }}
+        />
+      ))}
       <div
         className="circle circle--small"
         style={{
@@ -152,7 +108,32 @@ const Tunnel = () => {
           height: 40,
         }}
       >
-        <div className="circle circle--expanding" />
+        <div
+          className="circle circle--expanding"
+          style={{ width: '40px', height: '40px', top: '17px', left: '17px' }}
+        />
+      </div>
+      <div
+        className="circle circle--small"
+        style={{
+          left:
+            320 +
+            documentCenter.x -
+            circleSize / 1.6 +
+            distanceX / (circleSize / 1.6 / 20),
+          top:
+            320 +
+            +documentCenter.y -
+            circleSize / 1.6 +
+            distanceY / (circleSize / 1.6 / 20),
+          width: 30,
+          height: 30,
+        }}
+      >
+        <div
+          className="circle circle--expanding"
+          style={{ width: '30px', height: '30px', top: '12px', left: '12px' }}
+        />
       </div>
     </>
   )
