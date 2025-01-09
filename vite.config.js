3a337fdf0f0f4bc8308cff2a84158504d6efbf76
asdf <eemeli.paalimaki.work@protonmail.com>
2025-01-09 18:59:28 +0200

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    base: 'https://spherical-spinach.github.io/css-tunnel-effect/',
-  },
+  base: 'https://spherical-spinach.github.io/css-tunnel-effect/',
 })
